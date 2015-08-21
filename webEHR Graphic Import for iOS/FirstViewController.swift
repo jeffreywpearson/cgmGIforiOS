304ba62b0b08021c013abc57d116d8d855055eda
Jeffrey W. Pearson <jeffreywpearson@Jeffreys-MacBook-Pro.local>
2015-08-20 20:28:29 -0700

    Initial Commit

---
+++ webEHR Graphic Import for iOS/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  webEHR Graphic Import for iOS
+//
+//  Created by Jeffrey W. Pearson on 8/20/15.
+//  Copyright (c) 2015 Jeffrey W. Pearson. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ webEHR Graphic Import for iOS/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  webEHR Graphic Import for iOS
+//
+//  Created by Jeffrey W. Pearson on 8/20/15.
+//  Copyright (c) 2015 Jeffrey W. Pearson. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
