971402c3312beb984d25d71b2602e3fe77780100
Jeffrey W. Pearson <jeffreywpearson@Jeffreys-MacBook-Pro.local>
2015-08-20 20:29:42 -0700

    Initial commit.

--- webEHR Graphic Import for iOS/FirstViewController.swift
+++ webEHR Graphic Import for iOS/FirstViewController.swift
@@ -13,6 +13,8 @@ class FirstViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        println("I can't wait to push this project to GitHub.");
     }
 
     override func didReceiveMemoryWarning() {
